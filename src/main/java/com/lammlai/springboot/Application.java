cc9d73f869c36a2f47d6a5a18132e96ace928c24
laiminhlamgt <laiminhlamgt@gmail.com>
2019-12-16 17:25:38 +0700

    Implementing insert and update Spring JDBC Update Methods

--- src/main/java/com/lammlai/springboot/Application.java
+++ src/main/java/com/lammlai/springboot/Application.java
@@ -1,5 +1,6 @@
 package com.lammlai.springboot;
 
+import com.lammlai.springboot.entity.Person;
 import com.lammlai.springboot.jdbc.PersonJdbcDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -25,5 +28,7 @@ public class Application implements CommandLineRunner {
         logger.info("All users -> {}", personJdbcDao.findAll());
         logger.info("User id 10001 -> {}", personJdbcDao.findById(10001));
         logger.info("Deleting 10002 -> No of Rows Deleted - {}", personJdbcDao.deleteById(10002));
+        logger.info("Inserting 10004 -> {}", personJdbcDao.insert(new Person(10004, "Tara", "Berlin", new Date())));
+        logger.info("Updating 10003 -> {}", personJdbcDao.update(new Person(10003, "Pieter", "Utrecht", new Date())));
     }
 }
