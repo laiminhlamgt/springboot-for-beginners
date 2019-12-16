d7ff20533e8f1c6e9d895358092d8add247c7cc6
laiminhlamgt <laiminhlamgt@gmail.com>
2019-12-16 14:54:04 +0700

    Execute the findAll method using CommandLineRunner

--- src/main/java/com/lammlai/springboot/Application.java
+++ src/main/java/com/lammlai/springboot/Application.java
@@ -1,13 +1,27 @@
 package com.lammlai.springboot;
 
+import com.lammlai.springboot.jdbc.PersonJdbcDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    PersonJdbcDao personJdbcDao;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("All users -> {}", personJdbcDao.findAll());
+    }
 }
