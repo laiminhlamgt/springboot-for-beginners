d7391f814247025882faa4ed2ea131bee6e5d86c
laiminhlamgt <laiminhlamgt@gmail.com>
2019-12-17 10:04:56 +0700

    Implementing deleteById JPA Repository Method

--- src/main/java/com/lammlai/springboot/JpaApplication.java
+++ src/main/java/com/lammlai/springboot/JpaApplication.java
@@ -28,7 +28,7 @@ public class JpaApplication implements CommandLineRunner {
         logger.info("User id 10001 -> {}", personJpaRepository.findById(10001));
         logger.info("Inserting -> {}", personJpaRepository.insert(new Person("Tara", "Berlin", new Date())));
         logger.info("Updating 10003 -> {}", personJpaRepository.update(new Person(10003, "Pieter", "Utrecht", new Date())));
+        personJpaRepository.deleteById(10002);
 //        logger.info("All users -> {}", personJpaRepository.findAll());
-//        logger.info("Deleting 10002 -> No of Rows Deleted - {}", personJpaRepository.deleteById(10002));
     }
 }
