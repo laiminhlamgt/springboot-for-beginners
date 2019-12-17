912db1a256d916e0e1893845a14d806340326c26
laiminhlamgt <laiminhlamgt@gmail.com>
2019-12-17 08:43:11 +0700

    Implementing findById JPA Repository Method

---
+++ src/main/java/com/lammlai/springboot/JpaApplication.java
@@ -0,0 +1,34 @@
+package com.lammlai.springboot;
+
+import com.lammlai.springboot.entity.Person;
+import com.lammlai.springboot.jpa.PersonJpaRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class JpaApplication implements CommandLineRunner {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    PersonJpaRepository personJpaRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        logger.info("User id 10001 -> {}", personJpaRepository.findById(10001));
+//        logger.info("All users -> {}", personJpaRepository.findAll());
+//        logger.info("Deleting 10002 -> No of Rows Deleted - {}", personJpaRepository.deleteById(10002));
+//        logger.info("Inserting 10004 -> {}", personJpaRepository.insert(new Person(10004, "Tara", "Berlin", new Date())));
+//        logger.info("Updating 10003 -> {}", personJpaRepository.update(new Person(10003, "Pieter", "Utrecht", new Date())));
+    }
+}
---
+++ src/main/java/com/lammlai/springboot/jpa/PersonJpaRepository.java
@@ -0,0 +1,20 @@
+package com.lammlai.springboot.jpa;
+
+import com.lammlai.springboot.entity.Person;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public class PersonJpaRepository {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public Person findById(int id) {
+        return entityManager.find(Person.class, id);
+    }
+}
