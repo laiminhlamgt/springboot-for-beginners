bca4159910dd341d02061cf24f0b174b6f95ede9
laiminhlamgt <laiminhlamgt@gmail.com>
2019-12-19 14:42:41 +0700

    Step 06 - Writing a deleteByID method to delete an Entity

--- src/main/java/com/lammlai/springboot/Application.java
+++ src/main/java/com/lammlai/springboot/Application.java
@@ -25,5 +25,6 @@ public class Application implements CommandLineRunner {
     public void run(String... args) {
         Course course = repository.findById(10001L);
         logger.info("Course 10001 -> {}", course);
+        repository.deleteById(10001L);
     }
 }
--- src/main/java/com/lammlai/springboot/repository/CourseRepository.java
+++ src/main/java/com/lammlai/springboot/repository/CourseRepository.java
@@ -3,10 +3,12 @@ package com.lammlai.springboot.repository;
 import com.lammlai.springboot.entity.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
 @Repository
+@Transactional
 public class CourseRepository {
 
     @Autowired
@@ -20,7 +22,8 @@ public class CourseRepository {
 //        return null;
 //    }
 
-//    public void deleteById(Long id) {
-//
-//    }
+    public void deleteById(Long id) {
+        Course course = findById(id);
+        em.remove(course);
+    }
 }
