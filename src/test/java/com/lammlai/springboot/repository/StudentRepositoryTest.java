735cf324d1f0dbdb5b62e17f6eb1a47cf4ba0bd9
laiminhlamgt <laiminhlamgt@gmail.com>
2019-12-23 16:16:22 +0700

    Step 25 - OneToOne Mapping - Retrieving Student with Passport and Eager Fetch

---
+++ src/test/java/com/lammlai/springboot/repository/StudentRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.lammlai.springboot.repository;
+
+import com.lammlai.springboot.Application;
+import com.lammlai.springboot.entity.Student;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+class StudentRepositoryTest {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    StudentRepository repository;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void retrieveStudentAndPassportDetails() {
+        Student student = em.find(Student.class, 20001L);
+        logger.info("student -> {}", student);
+        logger.info("passport -> {}", student.getPassport());
+    }
+}
