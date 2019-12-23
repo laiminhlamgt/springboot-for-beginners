aa4e81bd918eb1e05ad10866224a63513c8d4e8d
laiminhlamgt <laiminhlamgt@gmail.com>
2019-12-23 17:18:46 +0700

    Step 28 - OneToOne Mapping - Bidirectional Relationship - Part 1

--- src/main/java/com/lammlai/springboot/entity/Passport.java
+++ src/main/java/com/lammlai/springboot/entity/Passport.java
@@ -1,9 +1,6 @@
 package com.lammlai.springboot.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.StringJoiner;
 
 @Entity
@@ -15,6 +12,9 @@ public class Passport {
 
     @Column(nullable = false)
     private String number;
+
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "passport")
+    private Student student;
 
     protected Passport() {
     }
@@ -33,6 +33,14 @@ public class Passport {
 
     public void setNumber(String number) {
         this.number = number;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
     }
 
     @Override
