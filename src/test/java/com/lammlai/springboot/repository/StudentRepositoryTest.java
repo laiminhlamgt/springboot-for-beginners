08bbbd976706f8373de86df23bd26d1e56e62040
laiminhlamgt <laiminhlamgt@gmail.com>
2019-12-23 15:43:22 +0700

    Establishing Relationships with JPA and Hibernate

---
+++ src/test/java/com/lammlai/springboot/repository/StudentRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.lammlai.springboot.repository;
+
+import com.lammlai.springboot.Application;
+import com.lammlai.springboot.entity.Passport;
+import com.lammlai.springboot.entity.Student;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+class StudentRepositoryTest {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    StudentRepository repository;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void someTest() {
+        repository.someOperationToUnderstandPersistenceContext();
+    }
+
+    @Test
+    @Transactional
+    public void retrieveStudentAndPassportDetails() {
+        Student student = em.find(Student.class, 20001L);
+        logger.info("student -> {}", student);
+        logger.info("passport -> {}", student.getPassport());
+    }
+
+    @Test
+    @Transactional
+    public void retrievePassportAndAssociatedStudent() {
+        Passport passport = em.find(Passport.class, 40001L);
+        logger.info("passport -> {}", passport);
+        logger.info("student -> {}", passport.getStudent());
+    }
+}
