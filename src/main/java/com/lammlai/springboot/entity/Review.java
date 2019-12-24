601c5a5ffa4401555221e589d499bcb77261bb8a
laiminhlamgt <laiminhlamgt@gmail.com>
2019-12-24 14:32:16 +0700

    Step 30 - Part 2 - ManyToOne Mapping - Implementing the Mapping

--- src/main/java/com/lammlai/springboot/entity/Review.java
+++ src/main/java/com/lammlai/springboot/entity/Review.java
@@ -3,6 +3,7 @@ package com.lammlai.springboot.entity;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import java.util.StringJoiner;
 
 @Entity
@@ -15,6 +16,9 @@ public class Review {
     private String rating;
 
     private String description;
+
+    @ManyToOne
+    private Course course;
 
     protected Review() {
     }
@@ -42,6 +46,14 @@ public class Review {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
     }
 
     @Override
