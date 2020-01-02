381fa69dbba1f0d125b95924643897022487a830
laiminhlamgt <laiminhlamgt@gmail.com>
2020-01-02 16:41:33 +0700

    Step 44 - JPA Inheritance Hierachies and Mappings - Settings up a repository

--- src/main/java/com/lammlai/springboot/Application.java
+++ src/main/java/com/lammlai/springboot/Application.java
@@ -1,8 +1,11 @@
 package com.lammlai.springboot;
 
 import com.lammlai.springboot.entity.Course;
+import com.lammlai.springboot.entity.FullTimeEmployee;
+import com.lammlai.springboot.entity.PartTimeEmployee;
 import com.lammlai.springboot.entity.Student;
 import com.lammlai.springboot.repository.CourseRepository;
+import com.lammlai.springboot.repository.EmployeeRepository;
 import com.lammlai.springboot.repository.StudentRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.math.BigDecimal;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -21,6 +26,9 @@ public class Application implements CommandLineRunner {
 
     @Autowired
     private StudentRepository studentRepository;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -38,6 +46,10 @@ public class Application implements CommandLineRunner {
 //        courseRepository.addReviewsForCourse(10003L, reviews);
 
 //        studentRepository.insertHardcodedStudentAndCourse();
-        studentRepository.insertStudentAndCourse(new Student("Jack"), new Course("Microservices in 100 Steps"));
+//        studentRepository.insertStudentAndCourse(new Student("Jack"), new Course("Microservices in 100 Steps"));
+
+        employeeRepository.insert(new FullTimeEmployee("Jack", new BigDecimal("10000")));
+        employeeRepository.insert(new PartTimeEmployee("Jill", new BigDecimal("50")));
+        logger.info("All Employees -> {}", employeeRepository.retrieveAllEmployees());
     }
 }
