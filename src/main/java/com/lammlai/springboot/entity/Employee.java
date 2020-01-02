7d9ea8288b5005d55c2d6a20b83c9d4168bdfaf7
laiminhlamgt <laiminhlamgt@gmail.com>
2020-01-02 16:54:53 +0700

    Step 44 - JPA Inheritance Hierachies and Mappings - Table Per Class

--- src/main/java/com/lammlai/springboot/entity/Employee.java
+++ src/main/java/com/lammlai/springboot/entity/Employee.java
@@ -4,8 +4,7 @@ import javax.persistence.*;
 import java.util.StringJoiner;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "EmployeeType")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Employee {
 
     @Id
