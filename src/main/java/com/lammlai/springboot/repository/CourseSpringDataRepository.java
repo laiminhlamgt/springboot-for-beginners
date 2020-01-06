f25263a8ccaf5620b64677563d2be25313069dfa
laiminhlamgt <laiminhlamgt@gmail.com>
2020-01-06 15:53:01 +0700

    Step 70 - Spring Data REST

--- src/main/java/com/lammlai/springboot/repository/CourseSpringDataRepository.java
+++ src/main/java/com/lammlai/springboot/repository/CourseSpringDataRepository.java
@@ -3,9 +3,11 @@ package com.lammlai.springboot.repository;
 import com.lammlai.springboot.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(path = "courses")
 public interface CourseSpringDataRepository extends JpaRepository<Course, Long> {
 
     List<Course> findByName(String name);
