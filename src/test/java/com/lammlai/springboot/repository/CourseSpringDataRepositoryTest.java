6c8f6a2e67e20f7d5b3c588a730d28b40f6a449a
laiminhlamgt <laiminhlamgt@gmail.com>
2020-01-06 15:18:14 +0700

    Step 68 - Pagination using Spring Data JPA Repository

--- src/test/java/com/lammlai/springboot/repository/CourseSpringDataRepositoryTest.java
+++ src/test/java/com/lammlai/springboot/repository/CourseSpringDataRepositoryTest.java
@@ -8,10 +8,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Arrays;
 import java.util.Optional;
 
 import static org.junit.Assert.assertFalse;
@@ -55,5 +57,16 @@ public class CourseSpringDataRepositoryTest {
         Sort sort = Sort.by(Sort.Direction.ASC, "name");
         logger.info("Sorted Courses -> {}", repository.findAll(sort));
         logger.info("Count -> {}", repository.count());
+    }
+
+    @Test
+    public void pagination() {
+        PageRequest pageRequest = PageRequest.of(0, 3);
+        Page<Course> firstPage = repository.findAll(pageRequest);
+        logger.info("First Page -> {}", firstPage.getContent());
+
+        Pageable secondPageable = firstPage.nextPageable();
+        Page<Course> secondPage = repository.findAll(secondPageable);
+        logger.info("Second Page -> {}", secondPage.getContent());
     }
 }
